492387056507b4f2ba7a25215b188038a38e1a74
Amit Damor <amitdamor2648@gmail.com>
2025-11-13 15:25:48 +0530

    make object of crud files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world...");
+        Scanner scan=new Scanner(System.in);
+
+        addStudent aStudent=new addStudent();
+        updateStudent uStudent=new updateStudent();
+        retrieveStudent rStudent=new retrieveStudent();
+        dltStudent rStudent=new dltStudent();
+
     }
 }
